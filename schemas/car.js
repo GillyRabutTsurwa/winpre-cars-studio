48b741b971bab6b728feb900efc585de5fc923c7
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-01-29 17:56:51 -0500

    Added New type & priceLevel fields - added new, mandatory fields that are radio buttons reflecting the type and the price level of a car - also learnt that you can have a list of string types and whichever one is selected, is whose value will appear in the data. so it is just like we were doing with the input, but more specific and convenient in my use case

--- schemas/car.js
+++ schemas/car.js
@@ -20,6 +20,27 @@ export default defineType({
       title: 'Year',
       type: 'number',
     }),
+    /**
+     * NEW: added new field
+     * thanks to https://www.sanity.io/docs/string-type for reference
+     */
+    defineField({
+      name: 'type',
+      title: 'Type',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Sedan', value: 'sedan'},
+          {title: 'SUV', value: 'suv'},
+          {title: 'Hatchback', value: 'hatchback'},
+          {title: 'Sports Car', value: 'sports-car'},
+          {title: 'Pick-Up Truck', value: 'truck'},
+        ],
+        layout: 'radio',
+      },
+      // NOTE: making these required. cannot publish sans selectionner
+      validation: (Rule) => Rule.required(),
+    }),
     defineField({
       name: 'slug',
       title: 'Slug',
@@ -32,6 +53,25 @@ export default defineType({
       title: 'Price',
       name: 'price',
       type: 'number',
+    }),
+    defineField({
+      // NEWNOTE: added new field, same schema as the new one above
+      title: 'Price Level',
+      name: 'priceLevel',
+      type: 'string',
+      options: {
+        list: [
+          // NOTE; not ideal to add the helper info to the title, but it doesn't show in the data, so its OK for now
+          {title: 'One ($4,999 or Less)', value: 'first'},
+          {title: 'Two ($5,000 to $9,999)', value: 'second'},
+          {title: 'Three ($10,000 to $14,999)', value: 'third'},
+          {title: 'Four ($15,000 to $19,999)', value: 'fourth'},
+          {title: 'Five ($20,000 to $24,999)', value: 'fifth'},
+          {title: 'Six ($25,000 or More)', value: 'sixth'},
+        ],
+        layout: 'radio',
+        validation: (Rule) => Rule.required(),
+      },
     }),
     defineField({
       title: 'Car Data',
