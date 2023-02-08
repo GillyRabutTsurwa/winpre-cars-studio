081ad1830a778a09af3fe98fae7370a316bdc73f
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-02-07 21:38:09 -0500

    Added New Schema for Image Slideshow

--- schemas/index.js
+++ schemas/index.js
@@ -2,5 +2,6 @@ import blockContent from './blockContent'
 import car from './car'
 import carData from './carData'
 import introText from './introText'
+import bannerImage from './bannerImage'
 
-export const schemaTypes = [blockContent, car, carData, introText]
+export const schemaTypes = [blockContent, bannerImage, car, carData, introText]
